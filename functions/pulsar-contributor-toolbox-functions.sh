b2b748575fb474e28932e593729d96e96397f31b
Lari Hotari <lari@hotari.net>
2021-02-12 13:48:39 +0200

    Speed up building core-modules with "-T 1C", 1 thread per core

--- functions/pulsar-contributor-toolbox-functions.sh
+++ functions/pulsar-contributor-toolbox-functions.sh
@@ -34,7 +34,7 @@ function ptbx_build_coremodules() {
   (
     ptbx_cd_git_root
     ptbx_clean_snapshots
-    mvn -Pcore-modules clean install -DskipTests -Dspotbugs.skip=true -Dassembly.skipAssembly=true
+    mvn -Pcore-modules -T 1C clean install -DskipTests -Dspotbugs.skip=true -Dassembly.skipAssembly=true
   )
 }
 
