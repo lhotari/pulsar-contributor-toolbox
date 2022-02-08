3d2c9e6468cd32f085f55ef8fb06d2dc8715f08a
Lari Hotari <lari@hotari.net>
2022-02-08 12:03:22 +0200

    Fix ptbx_project_version

--- functions/pulsar-contributor-toolbox-functions.sh
+++ functions/pulsar-contributor-toolbox-functions.sh
@@ -381,7 +381,7 @@ function ptbx_upload_log_to_gist() {
 function ptbx_project_version() {
   # prints out the project version and nothing else
   # https://maven.apache.org/plugins/maven-help-plugin/evaluate-mojo.html#forceStdout
-  mvn initialize help:evaluate -Dexpression=project.version -pl . -q -DforceStdout | sed 's/\[INFO\] \[stdout\] //' | grep -v '[WARN]' | tail -1
+  mvn initialize help:evaluate -Dexpression=project.version -pl . -q -DforceStdout | sed 's/\[INFO\] \[stdout\] //' | grep -F -v '[WARN]' | tail -1
 }
 
 function ptbx_build_docker_pulsar_all_image() {
