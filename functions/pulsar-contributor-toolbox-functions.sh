b3896aaa0ff300dd09e5d4b8fccfd919521eff8f
Lari Hotari <lari@hotari.net>
2023-02-03 08:47:01 +0200

    Add maintenance branches to sync

--- functions/pulsar-contributor-toolbox-functions.sh
+++ functions/pulsar-contributor-toolbox-functions.sh
@@ -318,9 +318,9 @@ function _ptbx_git_sync_branches() {
 function ptbx_git_sync_pulsar_maintenance_branches_with_upstream() {
   (
     cd ~/workspace-pulsar/pulsar
-    _ptbx_git_sync_branches origin master branch-2.7 branch-2.8 branch-2.9 branch-2.10
+    _ptbx_git_sync_branches origin master branch-2.7 branch-2.8 branch-2.9 branch-2.10 branch-2.11 branch-3.0
     cd ~/workspace-pulsar/pulsar.datastax
-    _ptbx_git_sync_branches datastax 2.7.2_ds 2.8.0_ds 2.8.3_ds
+    _ptbx_git_sync_branches datastax 2.7.2_ds 2.8.0_ds 2.8.3_ds 2.10_ds
   )
 }
 
