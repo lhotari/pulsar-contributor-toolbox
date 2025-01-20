15e0de6be71f4e9618106298f41a49903d341473
Lari Hotari <lhotari@users.noreply.github.com>
2025-01-20 15:19:31 +0200

    Show download progress

--- scripts/validate_pulsar_release.sh
+++ scripts/validate_pulsar_release.sh
@@ -64,12 +64,12 @@ if [[ ! $LOCAL ]]; then
     for file in apache-pulsar-$VERSION-bin.tar.gz apache-pulsar-$VERSION-bin.tar.gz.asc \
     apache-pulsar-$VERSION-bin.tar.gz.sha512 apache-pulsar-$VERSION-src.tar.gz apache-pulsar-$VERSION-src.tar.gz.asc \
     apache-pulsar-$VERSION-src.tar.gz.sha512; do
-        wget -nv -c $BASE_URL/$file
+        wget -c $BASE_URL/$file
     done
 
     for file in pulsar-io-cassandra-$VERSION.nar pulsar-io-cassandra-$VERSION.nar.asc \
     pulsar-io-cassandra-$VERSION.nar.sha512; do
-        wget -nv -c $BASE_URL/connectors/$file
+        wget -c $BASE_URL/connectors/$file
     done
 
     # Import the Pulsar KEYS
